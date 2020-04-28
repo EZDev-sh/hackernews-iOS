b6c075f0e5d2edc36a54ae15cb862364499b2800
EZDev-sh <ezdev.sh@gmail.com>
2020-04-28 17:36:19 +0900

    readme에 버전, 로그인 구현 계획 기입

--- Hackernews/Datas/APIMgr.swift
+++ Hackernews/Datas/APIMgr.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import SwiftSoup
+//import SwiftSoup
 
 class APIMgr {
     
@@ -211,79 +211,79 @@ class APIMgr {
         dataTask.resume()
     }
     
-    func loginHackerNews(id: String, pw: String) {
-        let baseURL = "https://news.ycombinator.com/login"
-        
-        guard let url = URL(string: baseURL) else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.setValue("*", forHTTPHeaderField: "Access-Control-Allow-Origin")
-        let body = "acct=\(id)&pw=\(pw)&goto=news".data(using: .utf8, allowLossyConversion: true)
-        request.httpBody = body
-        
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        
-        
-        let dataTask = session.dataTask(with: request) { (data, response, error) in
-            // client error check
-            if let clientError = error {
-                print(clientError)
-                return
-            }
-            
-            // server error check
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
-            guard (200..<300).contains(statusCode) else {
-                print("~~> status code : \(statusCode)")
-                // error handle
-                return
-            }
-            if let getData = data {
-                
-                if let html = String(data: getData, encoding: .utf8) {
-                    self.parseHtml(html: html)
-                }
-                
-            }
-        }
-        dataTask.resume()
-        
-    }
-    
-    func parseHtml(html: String) {
-        var userInfo: [AnyHashable: Any] = [:]
-        
-        do {
-            let doc = try SwiftSoup.parse(html)
-            let id = try doc.select("a")
-            let body = try doc.select("body").first()
-   
-            if let text = try body?.text() {
-                print(text)
-                if let _ = text.range(of: "Bad login.") {
-                    userInfo["loginResult"] = false
-                }
-                else {
-                    userInfo["loginResult"] = true
-                    for i in id {
-                        if i.id() == "me" {
-                            let userID = try i.text()
-                            self.userName = userID
-                            print(userID)
-                        }
-                    }
-                }
-            }
-            print(userInfo)
-            NotificationCenter.default.post(name: APIMgr.loginResult, object: nil, userInfo: userInfo)
-        } catch let htmlParserError {
-            print(htmlParserError.localizedDescription)
-
-        }
-        
-    }
+//    func loginHackerNews(id: String, pw: String) {
+//        let baseURL = "https://news.ycombinator.com/login"
+//
+//        guard let url = URL(string: baseURL) else { return }
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+//        request.setValue("*", forHTTPHeaderField: "Access-Control-Allow-Origin")
+//        let body = "acct=\(id)&pw=\(pw)&goto=news".data(using: .utf8, allowLossyConversion: true)
+//        request.httpBody = body
+//
+//        let config = URLSessionConfiguration.default
+//        let session = URLSession(configuration: config)
+//
+//
+//        let dataTask = session.dataTask(with: request) { (data, response, error) in
+//            // client error check
+//            if let clientError = error {
+//                print(clientError)
+//                return
+//            }
+//
+//            // server error check
+//            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
+//            guard (200..<300).contains(statusCode) else {
+//                print("~~> status code : \(statusCode)")
+//                // error handle
+//                return
+//            }
+//            if let getData = data {
+//
+//                if let html = String(data: getData, encoding: .utf8) {
+//                    self.parseHtml(html: html)
+//                }
+//
+//            }
+//        }
+//        dataTask.resume()
+//
+//    }
+//
+//    func parseHtml(html: String) {
+//        var userInfo: [AnyHashable: Any] = [:]
+//
+//        do {
+//            let doc = try SwiftSoup.parse(html)
+//            let id = try doc.select("a")
+//            let body = try doc.select("body").first()
+//
+//            if let text = try body?.text() {
+//                print(text)
+//                if let _ = text.range(of: "Bad login.") {
+//                    userInfo["loginResult"] = false
+//                }
+//                else {
+//                    userInfo["loginResult"] = true
+//                    for i in id {
+//                        if i.id() == "me" {
+//                            let userID = try i.text()
+//                            self.userName = userID
+//                            print(userID)
+//                        }
+//                    }
+//                }
+//            }
+//            print(userInfo)
+//            NotificationCenter.default.post(name: APIMgr.loginResult, object: nil, userInfo: userInfo)
+//        } catch let htmlParserError {
+//            print(htmlParserError.localizedDescription)
+//
+//        }
+//
+//    }
 }
 
 
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Hacker News for iOS
 
 __Hacker News API를 활용하여 REAST API를 연습하기위한 프로젝트입니다.__
+- iOS version : 11.0
 
 ## 사용한 API
 * Hacker News API => jobs 목록을 가져오기 위해사용
@@ -11,7 +12,7 @@ __Hacker News API를 활용하여 REAST API를 연습하기위한 프로젝트�
 
 ## 추후 개발 내용
 * 회원가입
-* 로그인
+* 로그인 (swiftsoup를 활용해서)
 * 댓글달기
 * 좋아요? (vote) 추가
 * 글쓰기
